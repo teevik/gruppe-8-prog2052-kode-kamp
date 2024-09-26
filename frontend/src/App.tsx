dc950f201c406730eed02ecf0f69df5fe57f583a
Ulrik Hesmyr <ulrik.hesmyr2002@gmail.com>
2024-09-26 15:39:10 +0200

    merge app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,15 @@
-<<<<<<< HEAD
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {SocketClient} from './socket-client/SocketClient'
-
-function App() {
-  const [count, setCount] = useState(0)
-=======
 import React from "react";
 import "./App.css";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landing/LandingPage";
 import GamePage from "./pages/game/GamePage";
-import ResultsPage from "./pages/results/resultsPage";
+import ResultsPage from "./pages/results/ResultsPage";
 // Import the Nav component, will see what is best here, import it here or import it in the pages...
 // import Nav from "./components/Nav"; 
->>>>>>> 7-spill-side-med-editor-og-kodeproblem
 
 const App: React.FC = () => {
   return (
-<<<<<<< HEAD
-    <>
-      <SocketClient/>
-    </>
-  )
-}
-=======
->>>>>>> 7-spill-side-med-editor-og-kodeproblem
 
     <Router>
       {/* Nav component will be present on all routes */}

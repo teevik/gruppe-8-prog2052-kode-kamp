111358f4d7fc0f9e08cbf818370713e1585fa110
Ulrik Hesmyr <ulrik.hesmyr2002@gmail.com>
2024-09-28 00:39:02 +0200

    Change middleware cors dependent on ENV

--- backend/src/middleware/socket.ts
+++ backend/src/middleware/socket.ts
@@ -1,7 +1,14 @@
 import type {Request, Response, NextFunction} from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const {ENV} = process.env;
 
 function socketAllowOrigin(req: Request, res : Response, next : NextFunction){
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    if(ENV && ENV == "dev"){
+        res.setHeader("Access-Control-Allow-Origin", "*");
+    }
     next();
 }
 

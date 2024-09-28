302ef481dce18635a3d987934dc269b2b52ada9c
Ulrik Hesmyr <ulrik.hesmyr2002@gmail.com>
2024-09-29 00:24:50 +0200

    Fikset run build frontend

--- frontend/src/pages/game/gamepage.test.ts
+++ frontend/src/pages/game/gamepage.test.ts
@@ -1,9 +1,9 @@
 import {test, expect} from 'vitest'
-import {getTimer} from '../../components/CountDown'
+import {formatSeconds} from '../../components/CountDown'
 
 test('Test countdown converter', ()=>{
-    expect(getTimer(72)).toBe("1m 12s")
-    expect(getTimer(60)).toBe("1m 0s")
-    expect(getTimer(50)).toBe("50s")
-    expect(getTimer(0)).toBe("0s")
+    expect(formatSeconds(72)).toBe("1m 12s")
+    expect(formatSeconds(60)).toBe("1m 0s")
+    expect(formatSeconds(50)).toBe("50s")
+    expect(formatSeconds(0)).toBe("0s")
 })

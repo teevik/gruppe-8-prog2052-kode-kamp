542f4729b1734954c7567e79f7c5f8938d70f048
Ulrik Hesmyr <ulrik.hesmyr2002@gmail.com>
2024-10-17 16:48:55 +0200

    Add insert scoreboard tests

--- backend/src/socketio/game.test.ts
+++ backend/src/socketio/game.test.ts
@@ -1,5 +1,5 @@
 import {test, expect} from 'vitest'
-import {binaryUpdateScoreboard} from './game'
+import {binaryUpdateScoreboard, fastestCodeUpdateScoreboard} from './game'
 import type { Participant } from '../../../shared/types'
 
 
@@ -19,17 +19,44 @@ test("Testing scoreboard binary search and insert function w only different exec
     expect(updateScoreboard[3]).toBe(scoreboard[2]);
 })
 
-test("Testing scoreboard binary search and insert function w some that have same execution time", () => {
+test("Testing edge-case scoreboard binary search and insert function w only different execution time", () => {
     //Creates mock data for current scoreboard (this must be sorted)
-    let scoreboard : Participant[] = [
-        {stats: {executionTime:1000, usedTime: 428743248}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}},
-        {stats: {executionTime:2000, usedTime: 428743248}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}},
-        {stats: {executionTime:3000, usedTime: 428743248}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}},
-        {stats: {executionTime:4000, usedTime: 428743248}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}}
-    ];
+    let scoreboard : Participant[] = [];
+    scoreboard.push({stats: {executionTime:3000, usedTime: 428743248}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}});
 
     let newScoreboardEntry = {stats: {executionTime:2000, usedTime: 428743248}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}};
 
     let updateScoreboard = binaryUpdateScoreboard([...scoreboard], newScoreboardEntry);
+    expect(updateScoreboard[0]).toBe(newScoreboardEntry);
+    expect(updateScoreboard[1]).toBe(scoreboard[0]);
+})
+
+test("Testing scoreboard binary search and insert function w some that have same execution time", () => {
+    //Creates mock data for current scoreboard (this must be sorted)
+    let scoreboard : Participant[] = [
+        {stats: {executionTime:1000, usedTime: 0}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}},
+        {stats: {executionTime:2000, usedTime: 0}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}},
+        {stats: {executionTime:3000, usedTime: 0}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}},
+        {stats: {executionTime:4000, usedTime: 0}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}}
+    ];
+
+    let newScoreboardEntry = {stats: {executionTime:2000, usedTime: 0}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}};
+
+    let updateScoreboard = binaryUpdateScoreboard([...scoreboard], newScoreboardEntry);
     expect(updateScoreboard[2]).toBe(newScoreboardEntry);
+})
+
+test("Testing scoreboard insert function w different execution time", ()=>{
+    let scoreboard : Participant[] = [];
+    scoreboard.push({stats: {executionTime:1000, usedTime: 0}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}});
+    scoreboard.push({stats: {executionTime:3000, usedTime: 0}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}});
+    scoreboard.push({stats: {executionTime:4000, usedTime: 0}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}});
+
+    let newScoreboardEntry = {stats: {executionTime:2000, usedTime: 0}, solution: "", socket: {userID: "", emoji: "", complete: false, userName: ""}};
+
+    let updateScoreboard = fastestCodeUpdateScoreboard([...scoreboard], newScoreboardEntry);
+    expect(updateScoreboard[0]).toBe(scoreboard[0]);
+    expect(updateScoreboard[1]).toBe(newScoreboardEntry);
+    expect(updateScoreboard[2]).toBe(scoreboard[1]);
+    expect(updateScoreboard[3]).toBe(scoreboard[2]);
 })

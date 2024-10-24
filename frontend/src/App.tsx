3d685add1e6b2b84ee227c44a169a4b1a41a1017
Jakob <jmwiren@stud.ntnu.no>
2024-10-24 15:51:08 +0200

    Added userLogin and userRegister pages and components, has some checks for correct input, however i am under the impression this will be handled by the responsecodes we get from the Get and Post req, so these are placeholders for now. Modified the nav component to have easy access to these pages, and added the nessesary routes to the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,23 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landing/LandingPage";
+
 // Import the Nav component, will see what is best here, import it here or import it in the pages...
-// import Nav from "./components/Nav";
+import Nav from "./components/Nav";
+import LoginPage from "./pages/userLogin/LoginPage";
+import RegisterPage from "./pages/userRegister/RegisterPage";
 
 const App: React.FC = () => {
   return (
     <>
       <Router>
         {/* Nav component will be present on all routes */}
-        {/* <Nav /> */}
+        <Nav />
         <Routes>
           {/* Define routes */}
           <Route path="/" element={<LandingPage />} />
+          <Route path="/LoginPage" element={<LoginPage />} />
+          <Route path="/RegisterPage" element={<RegisterPage />} />
         </Routes>
       </Router>
     </>

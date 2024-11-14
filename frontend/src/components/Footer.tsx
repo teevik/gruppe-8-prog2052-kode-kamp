9a0510cc5b22344bbeaf41187eee34a48552ad9a
Ulrik Hesmyr <ulrik.hesmyr2002@gmail.com>
2024-11-14 11:37:19 +0100

    Remove padding footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,9 +3,8 @@ import TermsOfService from "../pages/terms/TermsOfService";
 export default function Footer() {
   return (
     <footer>
-      <p>© 2020 Your Company, Inc. All rights reserved.</p>
+      <p>© 2024 Kodekamp, Inc. All rights reserved.</p>
       <TermsOfService />
-      
     </footer>
   );
 }

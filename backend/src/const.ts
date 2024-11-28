a82f989e372d3c7ae37da892278a9cd6cd85ad4e
musicero <oliveta@stud.ntnu.no>
2024-11-28 19:54:38 +0100

    change mode-explanation countdown from 5 to 15 for demo

--- backend/src/const.ts
+++ backend/src/const.ts
@@ -1,7 +1,7 @@
 import { env } from "./env";
 
 const MAX_PLAYERS_PR_GAME: number = 8;
-const COUNTDOWN_LENGTH_SECONDS: number = 5;
+const COUNTDOWN_LENGTH_SECONDS: number = 15;
 const TIME_AT_ENDSCREEN_SECONDS: number = 180;
 const GAME_LENGTH_MINUTES: number = 10;
 const LOBBY_TIMER_SECONDS: number = 30;

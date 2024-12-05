63c735891b3144e827786dc22ded1167acdbb442
teevik <teemu.vikoren@gmail.com>
2024-12-05 13:21:09 +0100

    gamepage

--- frontend/src/pages/game/GamePage.tsx
+++ frontend/src/pages/game/GamePage.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
+import Resizable from "react-resizable-layout";
 import type {
   Challenge,
   Participant,
   SocketData,
   TestResults,
 } from "../../../../shared/types";
+import { Button } from "../../components/Button";
 import CodeEditor from "../../components/CodeEditor";
 import CountDown from "../../components/CountDown";
-import { socket } from "../../socket";
 import TestResultsComponent from "../../components/TestResults";
+import { socket } from "../../socket";
 import ResultPage from "../results/ResultsPage";
 import "./GamePage.css"; // Import the CSS file
 
@@ -17,6 +19,7 @@ export interface GameProps {
   gameMode: string;
   gameTime: number;
   player: SocketData | undefined;
+  defaultTestResults?: TestResults;
 }
 
 /**
@@ -30,6 +33,7 @@ export default function SpeedCodingPage({
   gameMode,
   gameTime,
   player,
+  defaultTestResults,
 }: GameProps) {
   const [code, setCode] = useState<string>(challenge?.template || "");
   const [amountTestsPassed, setAmountTestsPassed] = useState<string>("");
@@ -40,11 +44,12 @@ export default function SpeedCodingPage({
   const [showResultPage, setShowResultPage] = useState<boolean>(false);
   const [gameIsOver, setGameIsOver] = useState<boolean>(false);
 
-  const [testResults, setTestResults] = useState<TestResults | undefined>();
+  const [testResults, setTestResults] = useState<TestResults | undefined>(
+    defaultTestResults
+  );
 
   const [timeAtResultPage, setTimeAtResultPage] = useState<number>(0);
   function updateScoreboard(scores: Participant[]) {
-    console.log("Scoreboard is updated", scores);
     setScoreboard(scores);
   }
 
@@ -85,10 +90,24 @@ export default function SpeedCodingPage({
     };
   }, []);
 
-  //const [code, setCode] = useState<string>("");
+  useEffect(() => {
+    const mathJax = (window as any).MathJax;
+
+    if (typeof mathJax !== "undefined") {
+      mathJax.typeset();
+    }
+  }, [challenge]);
+
+  function runCode(code: string) {
+    socket.emit("runCode", code);
+  }
+
+  function submitCode(code: string) {
+    socket.emit("submitCode", code);
+  }
 
   return (
-    <div className="gamePageContainer">
+    <div className="layout gamePage">
       {showResultPage && (
         <ResultPage
           scoreboard={scoreboard}
@@ -101,92 +120,131 @@ export default function SpeedCodingPage({
       {challenge && !showResultPage && (
         <>
           {/* Header Section */}
-          <div className="gamePageHeader">
-            <div className="gamePageHeaderTitle">KodeKamp</div>
-            <div className="gamePageHeaderMode">
-              <p>gamemode</p>
-              <p className="gamePageHeaderTitle">{gameMode}</p>
+          <div className="header">
+            <h2>KodeKamp: {gameMode}</h2>
+
+            <div className="headerRow">
+              <CountDown initialCounter={gameTime} />
+
+              <Button
+                onClick={() => setCode(challenge.template)}
+                variant="secondary"
+              >
+                Reset
+              </Button>
+              <Button
+                variant="secondary"
+                disabled={submittedCode}
+                onClick={() => {
+                  if (code) {
+                    setSubmittedCode(true);
+                    runCode(code);
+                  }
+                }}
+              >
+                Run
+              </Button>
+              <Button
+                disabled={submittedCode}
+                onClick={() => {
+                  if (code) {
+                    setSubmittedCode(true);
+                    submitCode(code);
+                  }
+                }}
+              >
+                Submit
+              </Button>
             </div>
-            <CountDown initialCounter={gameTime} />
           </div>
 
           {/* Main Content Section */}
-          <div className="gamePageMain">
-            {/* Left Section: Task Description */}
-            <div className="gamePageTaskDescription">
-              <div className="gamePageTaskTitle">{challenge.title}</div>
+          <div className="mainContainer">
+            <Resizable axis={"x"} initial={600} min={300}>
+              {({ position, separatorProps }) => (
+                <>
+                  {/* Left Section: Task Description */}
+                  <div className="taskDescription" style={{ width: position }}>
+                    {testResults && (
+                      <TestResultsComponent testResults={testResults} />
+                    )}
 
-              {testResults && (
-                <TestResultsComponent testResults={testResults} />
-              )}
+                    <div className="taskTitle">{challenge.title}</div>
 
-              <div
-                dangerouslySetInnerHTML={{ __html: challenge.description }}
-              ></div>
-              <h2>Input:</h2>
-              <div dangerouslySetInnerHTML={{ __html: challenge.input }}></div>
-              <h2>Output:</h2>
-              <div dangerouslySetInnerHTML={{ __html: challenge.output }}></div>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: challenge.description,
+                      }}
+                    ></div>
+                    <h2>Input:</h2>
+                    <div
+                      dangerouslySetInnerHTML={{ __html: challenge.input }}
+                    ></div>
+                    <h2>Output:</h2>
+                    <div
+                      dangerouslySetInnerHTML={{ __html: challenge.output }}
+                    ></div>
 
-              <h2>Examples:</h2>
-              <section>
-                {challenge.sampleTests.map((test, index) => (
-                  <div key={index}>
-                    <section className="examples">
-                      <div>
-                        <p>Samle input {index + 1}:</p>
-                        <div className="io">
-                          {test.input.map((input) => (
-                            <p key={input}>{input}</p>
-                          ))}
+                    <h2>Examples:</h2>
+                    <section>
+                      {challenge.sampleTests.map((test, index) => (
+                        <div key={index}>
+                          <section className="examples">
+                            <div>
+                              <p>Samle input {index + 1}:</p>
+                              <div className="io">
+                                {test.input.map((input) => (
+                                  <p key={input}>{input}</p>
+                                ))}
+                              </div>
+                            </div>
+
+                            <div>
+                              <p>Sample output {index + 1}:</p>
+                              <div className="io">
+                                {test.output.map((output) => (
+                                  <p key={output}>{output}</p>
+                                ))}
+                              </div>
+                            </div>
+                          </section>
                         </div>
-                      </div>
-
-                      <div>
-                        <p>Sample output {index + 1}:</p>
-                        <div className="io">
-                          {test.output.map((output) => (
-                            <p key={output}>{output}</p>
-                          ))}
-                        </div>
-                      </div>
+                      ))}
                     </section>
+                    <cite>
+                      Attribution:
+                      {challenge.attribution.map((attr) => (
+                        <a key={attr.name} href={attr.url}>
+                          {attr.name}
+                        </a>
+                      ))}
+                    </cite>
+                    <p>
+                      License:
+                      {challenge.license}
+                    </p>
                   </div>
-                ))}
-              </section>
-              <cite>
-                Attribution:
-                {challenge.attribution.map((attr) => (
-                  <a key={attr.name} href={attr.url}>
-                    {attr.name}
-                  </a>
-                ))}
-              </cite>
-              <p>
-                License:
-                {challenge.license}
-              </p>
-            </div>
 
-            {/* Right Section: Code Editor */}
-            <div className="gamePageEditor">
-              <p className="testResults">
-                {amountTestsPassed !== "" &&
-                  `Tests passed: ${amountTestsPassed}`}
-              </p>
-              {submittedCode && <div className="loader"></div>}
-              <CodeEditor
-                code={code}
-                setCode={setCode}
-                template={challenge.template}
-                submittedCode={submittedCode}
-                setSubmittedCode={setSubmittedCode}
-              />
-            </div>
+                  <div className="separator" {...separatorProps} />
+                  {/* Right Section: Code Editor */}
+                  <div
+                    className="editor"
+                    style={{
+                      width: `calc(100% - ${position}px`,
+                    }}
+                  >
+                    <CodeEditor
+                      code={code}
+                      setCode={setCode}
+                      template={challenge.template}
+                      submittedCode={submittedCode}
+                      setSubmittedCode={setSubmittedCode}
+                    />
+                  </div>
+                </>
+              )}
+            </Resizable>
           </div>
-
-          {/* Footer Section */}
-          <div className="gamePageFooter"></div>
         </>
       )}
     </div>
